88bf52f3c95b3d376f6cb1146757b8a92c996c42
Astra <juan.ramirez.j99@gmail.com>
2022-12-06 20:19:56 -0600

    Montando un servidor con fastAPI y uvicorn

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,16 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/")
+def get_list():
+    return ["Product 1", "Product 2", "Product 3"]
+
+@app.get("/contact", response_class=HTMLResponse)
+def get_contact():
+    return "<h1>Esto es una pagina de contacto</h1>"
 
 def run():
     store.get_data()
